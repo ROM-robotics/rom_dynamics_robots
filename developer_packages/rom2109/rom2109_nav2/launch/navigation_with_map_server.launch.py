f30eb4bbfa8e4df2224670340064e4b31f6b0963
GhostMan <server01.psa1981@gmail.com>
2025-01-17 14:19:33 +0630

    update

--- developer_packages/rom2109/rom2109_nav2/launch/navigation_with_map_server.launch.py
+++ developer_packages/rom2109/rom2109_nav2/launch/navigation_with_map_server.launch.py
@@ -47,7 +47,8 @@ def generate_launch_description():
     # TODO(orduno) Substitute with `PushNodeRemapping`
     #              https://github.com/ros2/launch_ros/issues/56
     remapping = [('/tf', 'tf'),
-                  ('/tf_static', 'tf_static')]
+                  ('/tf_static', 'tf_static'),
+                  ('/diffbot_base_controller/odom', 'odom'),]
 
     # Create our own temporary YAML files that include substitutions
     param_substitutions = {
